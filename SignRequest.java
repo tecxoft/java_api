9179a1dd6c235171e9b4275f20614239e8cf015b
tecxoft <admin@tecxoft.com>
2015-03-22 21:27:22 +0500

    Update SignRequest.java

--- SignRequest.java
+++ SignRequest.java
@@ -4,19 +4,19 @@ import com.tecxoft.tips.api.TIPSSignRequest;
 public class SignRequest {
     public static void main(String args[]){
     	try{
-		    // initialize the class with userId and password
-	    	TIPSSignRequest request = new TIPSSignRequest("224488", "password"); 
-	    	// signed PDF file data received in byte array
-			byte[] data = request.sendSignRequest("C:/folder/testFile.pdf");
-			// choose to write the file to file system
-			FileOutputStream fileOutputStream = null;
-			fileOutputStream = new FileOutputStream("C:/folder/file"+System.currentTimeMillis()+"-java"+".pdf");
-			fileOutputStream.write(data);
-			fileOutputStream.close();
-			System.out.println("File written.");
+	    // initialize the class with userId and password
+	    TIPSSignRequest request = new TIPSSignRequest("224488", "password"); 
+	    // signed PDF file data received in byte array
+	    byte[] data = request.sendSignRequest("C:/folder/testFile.pdf");
+	    // choose to write the file to file system
+	    FileOutputStream fileOutputStream = null;
+	    fileOutputStream = new FileOutputStream("C:/folder/file"+System.currentTimeMillis()+"-java"+".pdf");
+	    fileOutputStream.write(data);
+	    fileOutputStream.close();
+	    System.out.println("File written.");
     	}
     	catch (Exception e) {
-			e.printStackTrace();
-		}
+	    e.printStackTrace();
+	}
     }
 }
